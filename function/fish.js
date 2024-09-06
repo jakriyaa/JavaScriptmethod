a975c6c5e42bef31b29947bd608158a18fec9835
jakariyaa <jakariyatalukdar8501@gmai.com>
2024-09-06 12:00:13 +0600

    adjustment

---
+++ function/fish.js
@@ -0,0 +1,12 @@
+function macherTorkari(mach, shobji, mosla){
+    // রান্নার নির্দেশাবলী কনসোল লগ
+    console.log("Tel gorom korun");
+    console.log("Mosla Din");
+    console.log("Saksobji Din");
+    console.log(mach+"Din");
+
+    // রান্না শেষে Return ভ্যালু।
+    return "Onek Mojar Macher Torkari"
+}
+const result=macherTorkari("Elish","Potato" ,"Holud")
+console.log(result)
---
+++ function/sum.js
@@ -0,0 +1,8 @@
+// Function Decleare 
+function sum(a,b){
+    const result=x+y;
+    return result;
+}
+// Function Call
+const total=(685,6546)
+console.log(total)
